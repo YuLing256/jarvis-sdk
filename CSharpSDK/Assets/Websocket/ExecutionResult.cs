34c3de628d8a314433ee8cf0ed38ccf253688a1f
YuLing <nathan.kb217@gmail.com>
2026-03-31 09:13:21 -0500

    Add ExecutionResult class for websocket responses

---
+++ CSharpSDK/Assets/Websocket/ExecutionResult.cs
@@ -0,0 +1,38 @@
+#nullable enable
+
+namespace ATCSharpSDK.Websocket;
+
+public class ExecutionResult
+{
+  public bool Successful;
+  public string Message;
+
+  public ExecutionResult(bool successfull, string? message)
+  {
+    if (string.IsNullOrEmpty(message))
+      message = "";
+
+    Successful = successful;
+    Message = message;
+  }
+
+  public static ExecutionResult Success(string? message = null)
+  {
+    return new ExecutionResult(true, message);
+  }
+
+  public static ExecutionResult Failure(string? message)
+  {
+    return new ExecutionResult(false, message);
+  }
+
+  public static ExecutionResult ServerFailure(string? message)
+  {
+      return Failure($"{message} {Strings.VedalFaultSuffix}");
+  }
+  
+  public static ExecutionResult ModFailure(string? message)
+  {
+      return Failure($"{message} {Strings.ModFaultSuffix}");
+  }
+}
